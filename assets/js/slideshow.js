e6f5c6e930f7a302ed81186d2156dff52d750963
Rufus Raghunath <rufusraghunath@Rufuss-MacBook-Pro.local>
2026-02-18 01:18:41 +0100

    Add slideshow

---
+++ assets/js/slideshow.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+let slideChangeInterval;
+
+function moveSlideshow(n) {
+  const newIndex = slideIndex + n;
+  const slides = document.getElementsByClassName("slide");
+
+  if (newIndex > slides.length) {
+    slideIndex = 1;
+  } else if (newIndex < 1) {
+    slideIndex = slides.length;
+  } else {
+    slideIndex = newIndex;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  moveSlideshow(0);
+  slideChangeInterval = setInterval(function () {
+    moveSlideshow(1);
+  }, 5000);
+});
+
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+
+  if (key === "ArrowLeft") {
+    moveSlideshow(-1);
+    clearInterval(slideChangeInterval);
+  } else if (key === "ArrowRight") {
+    moveSlideshow(1);
+    clearInterval(slideChangeInterval);
+  }
+});
